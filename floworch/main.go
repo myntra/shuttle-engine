73fe519db6faaa06e37aa3bc3325caae554e1834
shwetaprasad <shweta.prasad@myntra.com>
2019-11-08 00:14:57 +0530

    added http scrapper for both floworch and kuborch

--- floworch/main.go
+++ floworch/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/gorilla/mux"
 	"github.com/myntra/shuttle-engine/types"
 )
@@ -14,9 +19,39 @@ var MapOfDeleteChannelDetails = make(map[string]types.DeleteChannelDetails)
 
 func main() {
 	router := mux.NewRouter()
+
+	// intercepting http call and fetching/pushing metrics to localhost telegraf on 8181
+	httpWrapper := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mt := httpsnoop.CaptureMetrics(router, w, r)
+		timeParser, _ := time.ParseDuration(mt.Duration.String())
+		fmt.Printf("There are %.6f seconds in %v.\n", timeParser.Seconds(), timeParser)
+		rt := fmt.Sprintf("%.6f", timeParser.Seconds()*1000)
+		pushAppMetrics(r.URL.Path, r.Method, mt.Code, rt, mt.Written)
+	})
+
 	router.HandleFunc("/execute", executeHandler).Methods("Post")
 	router.HandleFunc("/callback", callbackHandler).Methods("Post")
 	port := 5500
 	log.Printf("Starting server on :%d", port)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), httpWrapper))
+}
+
+func pushAppMetrics(URL string, method string, statuscode int, responsetime string, writtenbytes int64) {
+
+	log.Printf("Request metrics :::  method=%s, url=%s, status_code=%d, response_time=%s, written_bytes=%d", method, URL, statuscode, responsetime, writtenbytes)
+
+	apiURL := "http://localhost:8181/telegraf?precision=ms"
+
+	pushData := `m_appmetrics,app_name=ci_floworch,url="` + URL + `",statuscode=` + strconv.Itoa(statuscode) + `,method="` + method + `" duration=` + responsetime
+	log.Println(pushData)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Post(apiURL, "text/plain", bytes.NewBufferString(pushData))
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf(resp.Status)
 }
--- kuborch/main.go
+++ kuborch/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/gorilla/mux"
 	"github.com/myntra/shuttle-engine/helpers"
 	"k8s.io/client-go/kubernetes"
@@ -29,6 +34,36 @@ func main() {
 	port := 5600
 	log.Printf("Starting up the server at %d", port)
 	router := mux.NewRouter()
+
+	// intercepting http call and fetching/pushing metrics to localhost telegraf on 8181
+	httpWrapper := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mt := httpsnoop.CaptureMetrics(router, w, r)
+		timeParser, _ := time.ParseDuration(mt.Duration.String())
+		fmt.Printf("There are %.6f seconds in %v.\n", timeParser.Seconds(), timeParser)
+		rt := fmt.Sprintf("%.6f", timeParser.Seconds()*1000)
+		pushAppMetrics(r.URL.Path, r.Method, mt.Code, rt, mt.Written)
+	})
+
 	router.HandleFunc("/executeworkload", executeWorkload).Methods("Post")
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), httpWrapper))
+}
+
+func pushAppMetrics(URL string, method string, statuscode int, responsetime string, writtenbytes int64) {
+
+	log.Printf("Request metrics :::  method=%s, url=%s, status_code=%d, response_time=%s, written_bytes=%d", method, URL, statuscode, responsetime, writtenbytes)
+
+	apiURL := "http://localhost:8181/telegraf?precision=ms"
+
+	pushData := `m_appmetrics,app_name=ci_kuborch,url="` + URL + `",statuscode=` + strconv.Itoa(statuscode) + `,method="` + method + `" duration=` + responsetime
+	log.Println(pushData)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Post(apiURL, "text/plain", bytes.NewBufferString(pushData))
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf(resp.Status)
 }
