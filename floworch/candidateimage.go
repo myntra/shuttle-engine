e841cb78e000d3056aa56695341963ff4fa376c3
Ritesh <ritesh.dash1@myntra.com>
2020-10-30 11:57:35 +0530

    candidateImage funnction was deleted, was used to update single param of a run

--- floworch/candidateimage.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/myntra/shuttle-engine/types"
-
-func updateKey(run *types.Run, parameter string, value string) {
-	run.parameter = value
-	updateRunDetailsToDB(run)
-}
